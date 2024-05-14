74eb33f6ab083ca5fe36747893b57a709014cc76
SudeepMishra <sudeepm0599@gmail.com>
2024-05-14 17:46:13 +0545

    ..

---
+++ app/loading.jsx
@@ -0,0 +1,10 @@
+import { RiLoader2Fill } from "@remixicon/react";
+
+
+export default function Loading() {
+    return (
+        <div className="grid">
+            <RiLoader2Fill />
+        </div>
+    );
+}
